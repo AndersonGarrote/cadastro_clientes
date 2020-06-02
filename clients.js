ea77d00edc121d853ae1b18c8316c779a5d84872
Anderson Garrote <ander.user@gmail.com>
2020-06-01 22:25:36 -0300

    Ajustes na formatação da saída

--- clients.js
+++ clients.js
@@ -18,7 +18,6 @@ function submitForm() {
     
     updateClientList();
     
-    //Clear fields
     document.getElementById("inputName").value = "";
     document.getElementById("inputEmail").value = "";;
     document.getElementById("inputPhone").value = "";
@@ -41,6 +40,7 @@ function removeClient(id) {
 
 
 function updateClientList() {
+    //Erase the previous clientsList
     document.getElementById("clientsList").innerHTML = "";
     
     if (clients.length > 0) {
@@ -54,47 +54,53 @@ function updateClientList() {
 }
 
 function createCard(value, idx){
-    
+    //Create Card
     var newCard = document.createElement("div");
     newCard.className = "card";
-
+    
     var cardBody = document.createElement("div");
     cardBody.className = "card-body";
     
+    //Client Name
     var cardTitle = document.createElement("h5");
     cardTitle.className = "card-title";
     
     cardTitle.innerText = value.name;
+    
+    cardBody.appendChild(cardTitle);
+    
+    //Client Info
+    addCardInfo("E-mail: " + value.email, cardBody);
+    addCardInfo("Telefone: " + value.phone, cardBody);
+    addCardInfo("CEP: " + value.cep, cardBody);
+    addCardInfo("Logradouro: " + value.street + ", " + "n. " + value.number, cardBody);
+    addCardInfo("Bairro: " + value.district, cardBody);
+    addCardInfo("Cidade: " + value.city + ", " + "Estado: " + value.state, cardBody);
 
-    var cardText = document.createElement("p");
-    cardText.className = "card-text"; 
-
-    cardText.textContent += "E-mail: " + value.email + " ";    
-    cardText.textContent += "Telefone: " + value.phone+ " ";
-    cardText.textContent += "CEP: " + value.cep + " ";
-    cardText.textContent += "Logradouro: " + value.street + " ";
-    cardText.textContent += "Número: " + value.number + " ";
-    cardText.textContent += "Bairro: " + value.district + " ";
-    cardText.textContent += "Cidade: " + value.city + " ";
-    cardText.textContent += "Estado: " + value.state + " ";
-        
+    //Add Button
     var cardButton = document.createElement("button");
     cardButton.className = "btn btn-danger";
     cardButton.onclick = function() {removeClientButtonPressed(idx)};
     cardButton.textContent = "Remover Cliente"; 
-
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
     cardBody.appendChild(cardButton);
+
+    //Add Card to ClientsList
     newCard.appendChild(cardBody);
     document.getElementById("clientsList").appendChild(newCard);
 }
 
 function showEmptyListMessage() {
     var infoText = document.createElement("div");
+    
     infoText.className = "alert alert-info"; 
-
     infoText.textContent += "Não há clientes cadastrados! Adicione clientes usando o formulário ao lado.";
     
     document.getElementById("clientsList").appendChild(infoText);
+}
+
+function addCardInfo(str, cardBody) {
+    var cardText = document.createElement("p");
+    cardText.className = "text";
+    cardText.textContent += str;
+    cardBody.appendChild(cardText);
 }
